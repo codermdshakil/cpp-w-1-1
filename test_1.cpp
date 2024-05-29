720c97094c088067573d4bd16d3e3b4b2a9203f4
codermdshakil <ahmedshakil0512@gmail.com>
2024-05-29 23:06:40 +0600

    pointer of pointer of a variable example

--- test_1.cpp
+++ test_1.cpp
@@ -1,9 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     int a = 3;
     int *p1 = &a;
-    
+
+    cout << &a << endl;
+    cout << p1 << endl;
+    cout << "a " << *p1 << endl;
+
+    int **p2 = &p1;
+    cout << &p1 << endl;  // p1 address
+    cout << p2 << endl;  // p1 address
+    cout << *p2 << endl;  // a address
+    cout << **p2 << endl; // a address value
+
     return 0;
 }
