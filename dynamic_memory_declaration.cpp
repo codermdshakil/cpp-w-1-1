3166944a6cd90f3036d014d9fddfa3861ab0b04a
codermdshakil <ahmedshakil0512@gmail.com>
2024-05-29 23:34:00 +0600

    dynamic variable and delete dymanic variable

---
+++ dynamic_memory_declaration.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    // dynamic variable declaration
+    int *a = new int;
+    * a = 5;
+
+    cout << *a  << endl;
+    delete a; // delete dynamic memory
+
+
+
+    // dynamic array declaration
+    return 0;
+}
--- test_1.cpp
+++ test_1.cpp
@@ -3,18 +3,23 @@ using namespace std;
 
 int main()
 {
-    int a = 3;
-    int *p1 = &a;
+//     int a = 3;
+//     int *p1 = &a;
 
-    cout << &a << endl;
-    cout << p1 << endl;
-    cout << "a " << *p1 << endl;
+    // cout << &a << endl;
+    // cout << p1 << endl;
+    // cout << "a " << *p1 << endl;
 
-    int **p2 = &p1;
-    cout << &p1 << endl;  // p1 address
-    cout << p2 << endl;  // p1 address
-    cout << *p2 << endl;  // a address
-    cout << **p2 << endl; // a address value
+    // int **p2 = &p1;
+    // cout << &p1 << endl;  // p1 address
+    // cout << p2 << endl;  // p1 address
+    // cout << *p2 << endl;  // a address
+    // cout << **p2 << endl; // a address value
+ 
+
+
+
+
 
     return 0;
 }
