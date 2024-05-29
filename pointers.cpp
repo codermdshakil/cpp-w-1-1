03500f5b76490e4f776aeb78dcf4cbbc8e3581b2
codermdshakil <ahmedshakil0512@gmail.com>
2024-05-29 14:56:52 +0600

    pointer declare and using pointer change variable value

--- pointers.cpp
+++ pointers.cpp
@@ -1,9 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     int a = 5;
-    int * a_ptr = &a;
-    
+
+    // create a pointer and stored other variable address
+    int *a_ptr = &a;
+
+    cout << a << endl;
+    cout << &a << endl;
+
+    // dereference using variable address get variable value
+    cout << *a_ptr << endl; // 5
+
+    int x = 100;
+    int *x_ptr = &x;
+
+    // using dereference change x variable value 
+    *x_ptr = 500;
+    cout << x << endl;
+
     return 0;
 }
+
+/**
+ * Summary
+ *  * pointer variable = value
+ *  & pointer variable = address
+ * 
+*/
