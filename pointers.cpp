a0e71fe1e6a97fdc4751838fe232f7a7669db6ea
codermdshakil <ahmedshakil0512@gmail.com>
2024-05-29 22:49:00 +0600

    pointer of a pointer of a variable

--- pointers.cpp
+++ pointers.cpp
@@ -3,23 +3,35 @@ using namespace std;
 
 int main()
 {
-    int a = 5;
+    // int a = 5;
 
     // create a pointer and stored other variable address
-    int *a_ptr = &a;
+    // int *a_ptr = &a;
 
-    cout << a << endl;
-    cout << &a << endl;
+    // cout << a << endl;
+    // cout << &a << endl;
 
     // dereference using variable address get variable value
-    cout << *a_ptr << endl; // 5
+    // cout << *a_ptr << endl; // 5
 
-    int x = 100;
-    int *x_ptr = &x;
+    // int x = 100;
+    // int *x_ptr = &x;
 
-    // using dereference change x variable value 
-    *x_ptr = 500;
-    cout << x << endl;
+    // using dereference change x variable value
+    // *x_ptr = 500;
+    // cout << x << endl;
+
+    int a = 5;
+    int *b = &a;
+
+    // store pointer of pointer address
+    int **c = &b;
+
+    cout << &a << endl; // 0x61ff08
+    cout << b << endl;  // 0x61ff08
+
+    // get value of a using **
+    cout << **c << endl; // 5
 
     return 0;
 }
@@ -29,5 +41,5 @@ int main()
  *  * pointer variable = value
  *  & pointer variable = address
  *  Dereference - Using variable address get variable value it's called Dereference
- * 
-*/
+ *
+ */
---
+++ test_1.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a = 3;
+    int *p1 = &a;
+    
+    return 0;
+}
